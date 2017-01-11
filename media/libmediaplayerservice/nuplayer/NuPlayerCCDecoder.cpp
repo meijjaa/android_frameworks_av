9ac90d41156f297f951bdcee1bd5d2cba3fcec72
Xiaoliang Wang <xiaoliang.wang@amlogic.com>
2017-01-11 15:05:12 +0800

    PD #132693: change for last commit compile error
    patch for http://scgit.amlogic.com:8080/#/c/17371/
    
    Change-Id: I118f1f1a51369c7476e04aa885bdec207a7df990

--- media/libmediaplayerservice/nuplayer/NuPlayerCCDecoder.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerCCDecoder.cpp
@@ -454,7 +454,7 @@ bool NuPlayer::CCDecoder::parseDTVCCPacket(int64_t timeUs, const uint8_t *data, 
         }
 
         if (block_size > 0) {
-            size_t trackIndex = getTrackIndex(kTrackTypeCEA708, service_number, &trackAdded);
+            //size_t trackIndex = getTrackIndex(kTrackTypeCEA708, service_number, &trackAdded);
             //if (mSelectedTrack == (ssize_t)trackIndex) {// wxl shield for 7.0 cts android.media.cts.MediaPlayerTest#testChangeSubtitleTrack 20161201
                 sp<ABuffer> ccPacket = new ABuffer(block_size);
                 memcpy(ccPacket->data(), br.data(), block_size);
